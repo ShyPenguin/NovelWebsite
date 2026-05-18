1a0648b8dcd681fdf974e843c8d3df1f98acfde9
ShyPenguin <domatojawad@gmail.com>
2026-05-18 10:19:28 +0800

    trust proxy

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -13,12 +13,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  }),
-);
+app.set("trust proxy", 1);
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
