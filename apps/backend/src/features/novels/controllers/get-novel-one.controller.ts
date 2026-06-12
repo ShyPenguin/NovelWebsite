69ffe38745bf6abb8436c7ef5c60ef2da2ca6494
ShyPenguin <domatojawad@gmail.com>
2026-06-12 20:04:34 +0800

    check the error in the stage

--- apps/backend/src/features/novels/controllers/get-novel-one.controller.ts
+++ apps/backend/src/features/novels/controllers/get-novel-one.controller.ts
@@ -8,6 +8,7 @@ export const getNovelOneController = async (
   req: AuthRequest,
   res: Response,
 ) => {
+  console.log("at get novel one controller");
   const cookie = createCookieWrapper(req, res);
 
   const user = await updateUserSessionExpiration(cookie);
@@ -16,11 +17,13 @@ export const getNovelOneController = async (
     req.user = user;
   }
 
+  console.log(user);
   const result = await getNovelByIdService(
     { id: Array.isArray(req.params.id) ? req.params.id[0] : req.params.id },
     req.user,
   );
 
+  console.log(result);
   return res.status(200).json({
     ok: true,
     data: result,
